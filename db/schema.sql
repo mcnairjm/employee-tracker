58094825ea3ebb28b3c149b01bcf79a84cc8963b
mcnairjm19 <mcnairjm1993@gmail.com>
2022-02-10 10:28:34 -0500

    new files and added schema

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30), NOT NULL,
+);
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER NOT NULL,
+);
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+);
