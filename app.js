ab861b61168048606463a219c706c6400270b641
mcnairjm19 <mcnairjm1993@gmail.com>
2022-02-11 11:25:53 -0500

    deleted connection.js and beginning inquier prompts

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const inquirer = require('inquirer');
-const db = require('./db/connection');
+//const db = require('./db/connection');
 const consoleTable = require('console.table');
+const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "company_db"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    homeScreen();
+  });
 
-
+  const homeScreen = () => {
+      inquirer
+        .prompt({
+            type: 'list',
+            name: 'tasks',
+            message: 'What would you like to do?',
+            choices: [
+                "View Departments",
+                "View Roles",
+                "View All Employees",
+                "Add a Department",
+                "Add a Role",
+                "Add an Employee",
+                "Update an Employee Role"
+            ] 
+        });
+  };
